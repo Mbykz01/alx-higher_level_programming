2905b61d63f96f1dfb91b11b8a441c6bbce5bd29
Mbykz01 <aminuabdulkarimibrahimu@gmail.com>
2022-08-31 10:06:29 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    another_list = []
-    for i in my_list:
-        if i == searother_list.append(replace)
-        else:i
-            another_list.append(i)
-    return another_list
