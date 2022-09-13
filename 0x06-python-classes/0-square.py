c7bbe75d0fce85c8c82ef419e5c73b1381942940
Mbykz01 <aminuabdulkarimibrahimu@gmail.com>
2022-09-13 10:44:21 -0700

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class Square:
+    def Square(self):
+        self.Square = Square
+        if Square == 0:
+        
+            return True
+       
+        else:
+            return False
+
+Square1 = Square()
+Square1.Square = 0
+
+print(Square1.Square)
