500095d7c48c9495ae24ee1878ea3966dfd211a2
Mbykz01 <aminuabdulkarimibrahimu@gmail.com>
2022-09-19 11:37:44 -0700

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class Rectangle:
     def rectangle(self):
-        if  self.rectangle == {}
-        return True
-my_rectangle.{} = Rectangle()
-print(my_rectangle.{})
+        self.retangle = my_rectangle
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
