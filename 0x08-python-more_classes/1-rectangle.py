b18aa691980b420be73768d2945456d9d5b0b585
Mbykz01 <aminuabdulkarimibrahimu@gmail.com>
2022-09-19 07:44:41 -0700

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+class Rectangle:
+
+    def rectangle(self):
+        def width(self, value):
+            self.value = width
+            try:
+                if width == int:
+                    raise TypeError
+            except:
+                print("width must be an integer")
+            try:
+                if width < 0:
+                    raise  ValueError
+            except:
+                print("width must be >= 0")
+            height = Rectangle()
+            def height(self, value):
+                self.height = height
+                self.value = value
+            try:
+                if height == int:
+                    raise TypeError
+            except:
+                print("height must be an integer")
+            try:
+                if height < 0:
+                    raise ValueError
+            except:
+                print("height must be >= 0")
+
+            def __init__(self, width=0, height=0):
+                self.width = width
+                self.height = height
+
+    rectangle.width = Rectangle()
+    rectangle.heigth = Rectangle()
+    print(rectangle.width)
+
