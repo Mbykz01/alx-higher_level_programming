22a72995f8ebfae256b5131d144267a46d18279e
Mbykz01 <aminuabdulkarimibrahimu@gmail.com>
2022-09-23 13:22:01 -0700

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,57 @@
 #!/usr/bin/python3
-'''Add integers module'''
+"""
+
+This module is composed by a function that adds two numbers
+
+"""
 
 
 def add_integer(a, b=98):
-    '''Add two numbers.
-
+    """ Function that adds two integer and/or float numbers
 
     Args:
-        a: first integer number
-        b: second integer number
-    Raises:
-        TypeError: If a or b is not an integer value
+        a: first number
+        b: second number
+
     Returns:
-        The addition of supplied arguments
-    '''
-    if (type(a) != int and type(a) != float):
+        The addition of the two given numbers
+
+    Raises:
+        TypeError: If a or b aren't integer and/or float numbers
+
+    """
+
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if (type(b) != int and type(b) != float):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    return int(a + b)
+    a = int(a)
+    b = int(b)
+    return (a + b)  #!/usr/bin/python3
+
+
+# This module is composed by a function that adds two numbers
+
+
+def add_integer(a, b=98):
+    """ Function that adds two integer and/or float numbers
+
+    Args:
+        a: first number
+        b: second number
+
+    Returns:
+        The addition of the two given numbers
+
+    Raises:
+        TypeError: If a or b aren't integer and/or float numbers
+
+    """
+
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
